74ad28e412426f498f5fdedc32a0bf3c661826d3
Mohamed Arafa <ghostarafa@gmail.com>
2017-04-15 14:44:52 +0200

    init: Home Page RecyclerView Initialization

---
+++ app/src/main/java/com/zeowls/redditu/HomeFragment.java
@@ -0,0 +1,75 @@
+package com.zeowls.redditu;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+    @BindView(R.id.home_recycler_view)
+    RecyclerView mHomeRecycler;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v =  inflater.inflate(R.layout.fragment_home, container, false);
+        ButterKnife.bind(this, v);
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
+        mHomeRecycler.setLayoutManager(mLayoutManager);
+        mHomeRecycler.setItemAnimator(new DefaultItemAnimator());
+        mHomeRecycler.setAdapter(new HomeAdapter());
+    }
+
+    class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyViewHolder> {
+
+
+        class MyViewHolder extends RecyclerView.ViewHolder {
+            MyViewHolder(View view) {
+                super(view);
+            }
+        }
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.main_list_item, parent, false);
+            return new MyViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+        }
+
+        @Override
+        public int getItemCount() {
+            return 20;
+        }
+    }
+
+}
--- app/src/main/java/com/zeowls/redditu/MainActivity.java
+++ app/src/main/java/com/zeowls/redditu/MainActivity.java
@@ -1,13 +1,22 @@
 package com.zeowls.redditu;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.container)
+    FrameLayout mContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        getSupportFragmentManager().beginTransaction().add(R.id.container ,new HomeFragment(), null).commit();
     }
 }
